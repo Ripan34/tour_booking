99e43b14f3fb4b17c94196e40af3827ae6fc7909
Ripandeep Singh <ripandeep.singh@sjsu.edu>
2023-09-30 21:47:23 -0700

    added carousel

---
+++ bains_tourism_backend/src/config/dbConfig.js
@@ -0,0 +1,17 @@
+/**
+ * TO_DO: put PROCESS.ENV
+ */
+const dbConfig = {
+    database: 'bains_tourism',
+    user: 'postgres',
+    password: 'Pjr@343434',
+    host: '127.0.0.1',
+    port: '5432',
+    omitNull: true
+  }
+  const dialogFlowConfig = {
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDExhaS9JUc6RyA\n0W9zJBNVuILYUaIdGsQZWuEohaA0+BFSDmGElfqyFtbmPIM3ARuCR9Rak7XM4sYa\nL2eHfopGapc0IWC1vgwBfv4XYbyPczx4LgGaoOtJ5OmaJR0NzBe+JKKXMtCygf8q\nrpGbn66hxAngl3S059OMx/ATM64Wq+RassrNua/KdexaCXaLslO7wwBoHx1yXyp+\nLTgfRB8B64pupivMq7ruXfFZq1h/IGUXm2QGKsglkvx401BYFqOtBfJwVg4O+ukC\n19gq0n9zd3S9wy0kqHfwh/GYWJ2JrIXZQJnVcqWhloJstKYABVD75GBQIBxQLkUM\nutcEtWo/AgMBAAECggEALeq/h8lVJmrrQNAefrLTZDRrLAlYmAwP6I7BbdBIwzOD\n7y716JeK6ZXUzeMfB23VuoACeKRxTv84FRIsyXxozee/ouWLCL3xVUQKDPC8rIjK\neozZyPh2D0dAj7zLW9XdUHyCCGBOPOmC3rXfun33pT32xBbzn+ddAUQCrKhpnlZT\nMuhxQMD0EhMPHFyTaLu7AHmZS6mtqfn85LNtfv6bFiDZ5BIYsB3Ck8DfyTSBHy5X\nFWi0kz0iALZLcEkb7f0ilrc1oiGTxC/TYCj+rg+bljNQqSx7qdajtxCUh1bTdyCr\nkczpFxcsbQZTan5YFa+Fo/JnBLVjgUTtPBmWX6oE2QKBgQDwdWry7qB41/id9kXG\nnhLjZolBWOqPRdV5bRg7+gxnBS/PTwN7Zqs+GCQMTorxP/i8E/p3Vagc6YK9/uaW\nA/N9CIyOmf17fYbObCP45yBniYz0VVAidNj3Wxn07XJF8K9qFxEs6h0RipA223HN\ntjt7U0TqK/Sd2N8VmSSJeYvaewKBgQDRfd5uRjYkzQpm47pRHAXbS8qD1HugaMXo\nD9I1AMYODOen0vODJcOk3prs9cMKZZ1vqw/+Hc8IAjB7brd5raj5nX9CgLxXv7ZM\nxkVGlLG23Dl/nPFP2Yma1iPrseQaMn1FxGubQTRXcFF3OkCTSWj04xgJu7UbfXIP\n+v9WJMJWDQKBgF+27W5r7DaOCWmfSSEJdQ1NmF8JrLyBGLtv4JCnNN6hIunLUuCA\n+rMJ2veHICltxG3hKfbsgrGXhbJpT5HxWq+vgnuU8mCAk/OA+kSug6iTCSeM+gvt\nJRw5CDkFd1I9vyVkrzKIsi90T3l2mnM/EahCehEYlBNs60fmoLOUTUYXAoGBAMZm\nUWjYf+Cce1R0Kfk08+r7PYFak/aYI44ipa7i269k02PiahLzifUziUAPhLbtA4xF\nUrEUpATupeHQOl03dYdpfKOL7lOgUdMmF2e/3QrUSHQuO2+nEyYb1gsa/288D94q\nJsrzltGUouKqvYuAA7Gg/RwCtIOja4ns6TM5Pv9lAoGBAIz4xCNE/NhBSguNIK4e\nvKbF/HMQ4DMNZa7wJ1/JBgwXIMUYScvgBAAyyughPnO5jkDC6T1feuZBOjKU/kuB\n9+2+EDw5KxjGQPvSqCVLlW/rSiv5vjesf/aS79f6BGQ+n2WIZhIKijByJya9NzWz\nFMPnJ0Kc02d8dkYDhYSU7/zc\n-----END PRIVATE KEY-----\n",
+    "client_email": "dialogflow@bains-bot-xw9h.iam.gserviceaccount.com"
+}
+  exports.dbConfig = dbConfig;
+  exports.dialogFlowConfig = dialogFlowConfig;
